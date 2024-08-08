acdf4fec34abc61128cb085c9252b1047af70f26
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-08 05:02:45 -0700

    Add files via upload

---
+++ Problems/Panoramix's_Prediction.cs
@@ -0,0 +1,32 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Panoramix's Prediction
+ * Problem-Link : https://codeforces.com/problemset/problem/80/A
+*/
+
+using System;
+using System.Text.RegularExpressions;
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            int n, m;
+            Match match = Regex.Match(Console.ReadLine(),@"(\d+)\s(\d+)");
+            n = Convert.ToInt32(match.Groups[1].Value);
+            m = Convert.ToInt32(match.Groups[2].Value);
+            n += 1;
+            while (!Prime(n))
+                n++;
+            
+            Console.WriteLine(n==m?"YES":"NO");
+        }
+        private static bool Prime(int x) {
+            if (x <= 1)
+                return false;
+            for(int i = 2; i < x; i++) {
+                if (x % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
