7235446c4052d65ddc0b77c3f29d6db766e2b1cf
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-08 04:47:19 -0700

    Add files via upload

---
+++ Problems/Chips.cs
@@ -0,0 +1,24 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Chips
+ * Problem-Link : https://codeforces.com/problemset/problem/92/A
+*/
+
+using System;
+using System.Text.RegularExpressions;
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            int n, m, index;
+            Match match = Regex.Match(Console.ReadLine(), @"(\d+)\s(\d+)");
+            n = Convert.ToInt32(match.Groups[1].Value);
+            m = Convert.ToInt32(match.Groups[2].Value);
+            index = 1;
+            while (m>=index) {
+                m -= index;
+                index = index == n ? 1 : index + 1;
+            }
+            Console.WriteLine(m);
+        }
+    }
+}
