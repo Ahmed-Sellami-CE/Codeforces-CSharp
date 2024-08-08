41b7ac85ddc266b30050654fdd2a9dabc0ecd053
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-08 04:06:57 -0700

    Add files via upload

---
+++ Problems/Blackjack.cs
@@ -0,0 +1,17 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Blackjack
+ * Problem-Link : https://codeforces.com/problemset/problem/104/A
+*/
+
+using System;
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Cards(Convert.ToInt32(Console.ReadLine()) - 10));
+        }
+        private static int Cards(int x) {
+            return (x<=0 || x>11)?0 : x == 10?15:4;
+        }
+    }
+}
