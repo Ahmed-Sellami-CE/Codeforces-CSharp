ed254e44b3ad9156d8654717a8b360abd519a623
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-09 04:13:53 -0700

    Add files via upload

---
+++ Problems/Word.cs
@@ -0,0 +1,25 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Word
+ * Problem-Link : https://codeforces.com/problemset/problem/59/A
+*/
+
+using System;
+
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            string str = Console.ReadLine();
+            Console.WriteLine(IsLowerMajor(str) ? str.ToLower(): str.ToUpper());
+        }
+
+        static private bool IsLowerMajor(string str) {
+            int lowers = 0, uppers = 0;
+            foreach(char c in str){
+                uppers += (c >= 65 && c <= 90) ? 1 : 0;
+                lowers += (c >= 97 && c <= 122) ? 1 : 0;
+            }
+            return lowers >= uppers;
+        }
+    }
+}
