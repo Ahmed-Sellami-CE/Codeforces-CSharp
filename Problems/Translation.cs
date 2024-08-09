d22bdd2c45f1dde2d5a952c9029744999fecbfab
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-09 07:28:45 -0700

    Add files via upload

---
+++ Problems/Translation.cs
@@ -0,0 +1,28 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Translation
+ * Problem-Link : https://codeforces.com/problemset/problem/41/A
+*/
+
+using System;
+
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            string word1 = Console.ReadLine(),
+                word2= Console.ReadLine();
+            bool res=true;
+            if (word1.Length != word2.Length)
+                res = false;
+            else {
+                for(int i = 0; i < word1.Length; i++) {
+                    if (word1[i] != word2[word2.Length - i - 1]) {
+                        res = false;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(res?"YES":"NO");
+        }
+    }
+}
