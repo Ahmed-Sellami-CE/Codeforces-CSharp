b009c003150756801dac97e8dcee70eae71a58d4
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-09 06:38:46 -0700

    Add files via upload

---
+++ Problems/Domino_piling.cs
@@ -0,0 +1,19 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Domino piling
+ * Problem-Link : https://codeforces.com/problemset/problem/50/A
+*/
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            Match match = Regex.Match(Console.ReadLine(),@"(\d+)\s(\d+)");
+            int M = Convert.ToInt32(match.Groups[1].Value),
+                N = Convert.ToInt32(match.Groups[2].Value);
+            Console.WriteLine((M*N)/2);
+        }
+    }
+}
