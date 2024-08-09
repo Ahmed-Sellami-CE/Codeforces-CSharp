e92c828550da37de6c53e32ae139ef88ecb3be49
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-09 03:29:37 -0700

    Add files via upload

---
+++ Problems/Way_Too_Long_Words.cs
@@ -0,0 +1,22 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Way Too Long Words
+ * Problem-Link : https://codeforces.com/problemset/problem/71/A
+*/
+
+using System;
+
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string word,result;
+            result = "";
+            for(int i = 0; i < n; i++) {
+                word = Console.ReadLine();
+                result+=word.Length > 10 ? $"{word[0]}{word.Length-2}{word[word.Length-1]}\n":$"{word}\n";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
