223160f9cdb34073b2436a808c2cbdaa22fe40e6
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-09 07:12:36 -0700

    Add files via upload

---
+++ Problems/Triangular_numbers.cs
@@ -0,0 +1,23 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Triangular numbers
+ * Problem-Link : https://codeforces.com/problemset/problem/47/A
+*/
+
+using System;
+
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            int p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsSquare(8*p+1)?"YES":"NO");
+        }
+        static private bool IsSquare(int x) {
+            for(int i = 1; i <= x / 2; i++) {
+                if (i * i == x)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
