782451e5579c9dba3f4f308a65b9f4c86b7266d9
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-10 13:07:28 -0700

    Add files via upload

---
+++ Problems/Watermelon.cs
@@ -0,0 +1,20 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Watermelon
+ * Problem-Link : https://codeforces.com/problemset/problem/4/A
+*/
+
+using System;
+
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool result = false;
+            for(int i = 2; i <= n / 2 && !result; i+=2) {
+                result = (n - i) % 2 == 0;
+            }
+            Console.WriteLine(result ?"YES":"NO");
+        }
+    }
+}
