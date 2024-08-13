a5cc3a643bd07f5ab569345bfa14238cf3f01550
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-13 07:39:58 -0700

    Add files via upload

---
+++ Problems/Holiday_Of_Equality.cs
@@ -0,0 +1,27 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Holiday Of Equality
+ * Problem-Link : https://codeforces.com/problemset/problem/758/A
+*/
+
+using System;
+
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine()), index = 0;            string input = Console.ReadLine();
+            long[] burlers = new long[n];
+            long max = 0, result = 0;
+            while(index<n) {
+                burlers[index] = input.Contains(' ') ? Convert.ToInt64(input.Substring(0, input.IndexOf(' '))) : Convert.ToInt64(input);
+                input = input.Substring(input.IndexOf(' ')+1);
+                max = max < burlers[index] ? burlers[index] : max;
+                index++;
+            }
+            for(int i = 0; i < n; i++) {
+                result += max - burlers[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
