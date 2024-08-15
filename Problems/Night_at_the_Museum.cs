6873758b88e1ade7dd061fec6fd8ebb45cff8ef2
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2024-08-15 14:06:24 -0700

    Add files via upload

---
+++ Problems/Night_at_the_Museum.cs
@@ -0,0 +1,22 @@
+﻿/* 
+ * Author 	: Ahmed Sellami
+ * Problem-Name : A. Night at the Museum
+ * Problem-Link : https://codeforces.com/problemset/problem/731/A
+*/
+
+using System;
+
+namespace CodeForces {
+    internal class Program {
+        static void Main(string[] args) {
+            string input = Console.ReadLine();
+            char index = 'a';
+            int result = 0;
+            for(int i = 0; i < input.Length; i++) {
+                result += input[i] - index >= 0 ? Math.Min(26 - input[i] + index, input[i] - index) : Math.Min(26 - index + input[i], index - input[i]);
+                index = input[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
